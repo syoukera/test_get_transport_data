58c1cf354289fdf04f7819b4a5fc9fa7a570ff82
syoukera <s.akira2986@gmail.com>
2020-04-22 08:20:37 +0900

    change file configure

--- clean.sh
+++ clean.sh
@@ -5,15 +5,12 @@ rm ckinterp/ckinterpe
 rm tranfit/*.o
 rm tranfit/tranfite
 
-rm premix/*.o
-rm premix/premixe
+rm src/*.o
 
 rm link/*link
 
 rm output/*out
-rm output/recover
-rm output/restart
-rm output/save
 
 rm main.o
-rm main_e
+rm get_transport
+rm *.mod
--- compile.sh
+++ compile.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-gfortran -o premix/dmath.o -c premix/dmath.f -fPIC -fno-range-check
-gfortran -o premix/twopnt.o -c premix/twopnt.f
-gfortran -o premix/tranlib.o -c premix/tranlib.f
-gfortran -o premix/cklib.o -c premix/cklib.f
-gfortran -o premix/premix.o -c premix/premix.f
+gfortran -o src/dmath.o   -c src/dmath.f -fPIC -fno-range-check
+gfortran -o src/twopnt.o  -c src/twopnt.f
+gfortran -o src/tranlib.o -c src/tranlib.f
+gfortran -o src/cklib.o   -c src/cklib.f
 gfortran -c main.f90
 
-gfortran -o get_transport premix/premix.o main.o premix/cklib.o premix/tranlib.o premix/twopnt.o premix/dmath.o
+gfortran -o get_transport main.o src/cklib.o src/tranlib.o src/dmath.o
